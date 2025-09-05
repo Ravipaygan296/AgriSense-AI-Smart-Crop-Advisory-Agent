a533abfab091921b08eaa4bbd4c78ee67fb585c5
ravi payghan <payghanravi362@gmail.com>
2025-09-05 13:33:17 +0530

    Update pdf_loader.py

--- modules/rag_engine/pdf_loader.py
+++ modules/rag_engine/pdf_loader.py
@@ -1,1 +1,15 @@
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+def load_pdfs(folder="data/research_pdfs/"):
+    docs = []
+    for file in os.listdir(folder):
+        if file.endswith(".pdf"):
+            path = os.path.join(folder, file)
+            loader = PyPDFLoader(path)
+            documents = loader.load()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+            docs.extend(text_splitter.split_documents(documents))
+    return docs
 
